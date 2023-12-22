048489c17ac1dd4856955e19d2b8046ecc1f135a
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-12-22 21:23:28 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let name = [];
+
+  members.forEach((item) => {
+    if (typeof item === 'string') {
+      let string = item.replaceAll(' ', '').toUpperCase();
+      name.push(string.charAt(0));
+    }
+  })
+
+  let result = name.sort().join('');
+  
+  return result;
+} 
 
 module.exports = {
   createDreamTeam

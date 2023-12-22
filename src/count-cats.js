ace70edffae5284567ffa5f7a9d4e244d62d0b77
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-12-22 20:47:11 +0300

    feat: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let cat = '^^';
+  let cats = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    let row = matrix[i];
+
+    for (let j = 0; j < row.length; j++) {
+      if (row[j] === cat) {
+        cats++;
+      }
+    }
+  }
+
+  return cats;
 }
 
 module.exports = {

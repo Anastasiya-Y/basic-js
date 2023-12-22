24cd4fafc2eb1bbd6cdb590c72f78ac989a45c0a
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-12-22 23:52:46 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity === '') {
+    return false;
+  }
+
+  const num = +sampleActivity;
+  if (typeof num !== 'number' || Number.isNaN(num) || num > MODERN_ACTIVITY) {
+    return false;
+  } else if (num <= 0 || num > MODERN_ACTIVITY) {
+    return false;
+  }
+   
+  let k = Math.LN2 / HALF_LIFE_PERIOD;
+   
+  let t = (Math.log(MODERN_ACTIVITY / sampleActivity)) / k;
+
+  return Math.ceil(t);
 }
 
 module.exports = {

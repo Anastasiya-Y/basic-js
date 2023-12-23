20126806df1aa0b3a17b5230835806270267074e
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-12-23 16:37:44 +0300

    feat: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i - 1] === '--discard-next') {
+      newArr.push(undefined);
+      continue;
+    }
+    if (arr[i] === '--double-prev') {
+      newArr.push(newArr[i - 1]);
+      continue;
+    }
+    if (arr[i] === '--double-next') {
+      newArr.push(arr[i + 1]);
+      continue;
+    }
+    if (arr[i] === '--discard-prev') {
+      newArr.pop();
+      continue;
+    }
+    if (arr[i] === '--discard-next') {
+      newArr.push(undefined);
+      continue;
+    }
+  newArr.push(arr[i]);
+  }
+
+  const resultArr = newArr.filter((number) => number !== undefined);
+
+  return resultArr;
 }
 
 module.exports = {

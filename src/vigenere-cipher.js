060780ebd3380f08f71b7a049daf7e6c6ea3badb
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-12-26 02:49:23 +0300

    feat: add sum-digits and part of vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,9 +20,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getMatrix() {
+    let alphabet = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    let arr = [];
+    
+    for (let i = 0; i < alphabet.length; i++) {
+      let subArr = [];
+      for (let j = 0; j < alphabet.length; j++) {
+        subArr.push(alphabet[j]);
+      }
+      arr.push(subArr);
+      const letter = alphabet.shift();
+      alphabet.push(letter);
+    }
+    
+    return arr;
+  }
+
+  encrypt(string, key) {
+    if (!string || !key) {
+      throw new Error(`Incorrect arguments!`);
+    }
+    let result = '';
+    const matrix = this.getMatrix();
+
+    let newKey = '';
+    newKey = key.padStart(string.length, key);
+
+    for (let i = 0; i < string.length; i++) {
+      let strIndex = matrix.indexOf(string[i]);
+      let keyIndex = matrix.indexOf(key[i]);
+
+      result += matrix[strIndex][keyIndex];
+    }
+
+    return result;
   }
   decrypt() {
     throw new NotImplementedError('Not implemented');
